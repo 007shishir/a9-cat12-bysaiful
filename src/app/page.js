c2d53b3b6e4c2772701dd79c3ca3ec567639cbc2
007shishir <islam7.saiful@gmail.com>
2026-05-21 16:35:01 +0600

    added available room component

--- src/app/page.js
+++ src/app/page.js
@@ -1,11 +1,13 @@
 import Image from "next/image";
 import Hero from "./components/home/Hero";
+import AvailableRooms from "./components/home/AvailableRooms";
 
 export default function Home() {
   return (
     <div className="flex flex-col flex-1 items-center justify-center bg-zinc-50 font-sans dark:bg-black">
 
 <Hero/>
+<AvailableRooms/>
 
     </div>
   );
